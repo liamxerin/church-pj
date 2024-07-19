f10772a500bda04002128250e4ea23c7a49ccaea
brown <nyonaingloon327@gmail.com>
2024-07-19 13:52:19 +0700

    changed

--- server.js
+++ server.js
@@ -6,20 +6,32 @@ const mongoose = require('mongoose');
 const flash = require('connect-flash');
 const session = require('express-session');
 const adminRoutes = require('./routes/adminroute');
-const RedisStore = require('connect-redis').default; // Correct import
-const redisClient = require('redis').createClient();
 const multer = require('multer');
 const path = require('path');
 
 const app = express(); // Define app before using it
 
+// Set up session middleware (using default MemoryStore)
 app.use(session({
-  store: new RedisStore({ client: redisClient }),
   secret: 'myKey0203910293-13-10-0-210',
   resave: false,
   saveUninitialized: false,
 }));
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Directory where uploaded files will be stored
+  },
+  filename: (req, file, cb) => {
+    // Rename the file with a unique name (timestamp + originalname)
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+app.use('/uploads', express.static('uploads'));
 app.use(flash());
 
 // Middleware to parse JSON and URL-encoded data
@@ -32,31 +44,24 @@ app.use((req, res, next) => {
 });
 
 app.use(methodOverride('_method'));
-
-app.use('/uploads', express.static('uploads'));
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // Directory where uploaded files will be stored
-  },
-  filename: (req, file, cb) => {
-    // Rename the file with a unique name (timestamp + originalname)
-    cb(null, Date.now() + '-' + file.originalname);
-  }
-});
-
 app.use('/api/brothers/home', BrotherRoutes);
+
 app.get('/home', (req, res) => {
   res.render('home', { message: req.flash() });
 });
 
+// Uncomment and adjust if you have admin page
+// app.get('/admin_page', (req, res) => {
+//   res.render('admin_page', { message: req.flash() });
+// });
+
 app.use(adminRoutes);
 
-// Connect to the database and start the server
+// Connect to DB and start server
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT, () => {
-      console.log("Database connected and server is running on port", process.env.PORT);
+      console.log("Database-connected = 1, Listening port = 1");
     });
   })
   .catch((error) => {
