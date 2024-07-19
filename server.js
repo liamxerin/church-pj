f726c5e368f2fc097fda586654595f0dbaa745ba
brown <nyonaingloon327@gmail.com>
2024-07-19 13:37:19 +0700

    some updata

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 const flash = require('connect-flash');
 const session = require('express-session');
 const adminRoutes = require('./routes/adminroute');
-
+const RedisStore = require("connect-redis").default
 const RedisStore = require('connect-redis')(session);
 const redisClient = require('redis').createClient();
 
